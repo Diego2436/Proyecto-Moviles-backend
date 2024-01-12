32a3c2340b408f21e5826e43566ad0759c5052cc
Oscar David Romero <davidoconer555@gmail.com>
2024-01-12 15:29:01 -0600

    Modelos definitvos + PreCargado de Datos de Empleado, Productos y Tiendas

--- Proyecto_API/api/admin.py
+++ Proyecto_API/api/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Usuario, Tienda, Pedido, Producto, DetallesPedido
+from .models import FoodTruck, Usuario, Empleado, Pedido, Producto, DetallesPedido
 
 # Register your models here.
-
+admin.site.register(FoodTruck)
 admin.site.register(Usuario)
-admin.site.register(Tienda)
+admin.site.register(Empleado)
 admin.site.register(Pedido)
 admin.site.register(Producto)
 admin.site.register(DetallesPedido)
--- Proyecto_API/api/models.py
+++ Proyecto_API/api/models.py
@@ -1,35 +1,44 @@
 from django.db import models
 
-class Usuario(models.Model):
-    nombre = models.CharField(max_length=255, null=True)
-    direccion = models.CharField(max_length=255, null=True)
-    email = models.CharField(max_length=255, null=True)
-    telefono = models.CharField(max_length=15, null=True)
-    codigo = models.CharField(max_length=10, null=True)
-    verificacion = models.CharField(max_length=3, default='NO')
-    tipo = models.CharField(max_length=10, default='user')
+class FoodTruck(models.Model):
+    nombre = models.CharField(max_length=255, null=True, blank=True)
+    descripcion = models.TextField(null=True, blank=True)
 
-class Tienda(models.Model):
-    nombre = models.CharField(max_length=255, null=True)
-    descripcion = models.TextField(null=True)
+class Usuario(models.Model):
+    usuario = models.CharField(max_length=30, null=True, blank=True)
+    password = models.CharField(max_length=50, null=True, blank=True)
+    email = models.CharField(max_length=100, null=True, blank=True)
+    codigo = models.CharField(max_length=10, null=True, blank=True)
+    verificacion = models.CharField(max_length=3, default='NO')
+
+class Empleado(models.Model):
+    numeroEmpleado = models.CharField(max_length=20, null=True, blank=True)
+    password = models.CharField(max_length=50, null=True, blank=True)
+    email = models.CharField(max_length=100, null=True, blank=True)
+    nombre = models.CharField(max_length=50, null=True, blank=True)
+    rol = models.CharField(max_length=10, null=True, blank=True)
+    foodtruck = models.ForeignKey(FoodTruck, on_delete=models.CASCADE)
 
 class Pedido(models.Model):
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, null=True)
-    tienda = models.ForeignKey(Tienda, on_delete=models.CASCADE, null=True)
-    precio_total = models.DecimalField(max_digits=10, decimal_places=2, null=True)
-    fecha_pedido = models.CharField(max_length=30, null=True)
-    estado = models.CharField(max_length=20, null=True)
-    direccion = models.CharField(max_length=400, null=True)
+    precio_total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    fecha_pedido = models.CharField(max_length=45, null=True, blank=True)
+    fecha_entrega = models.CharField(max_length=45, null=True, blank=True)
+    fecha_repartidor = models.CharField(max_length=45, null=True, blank=True)
+    estado = models.CharField(max_length=20, null=True, blank=True)
+    direccion = models.CharField(max_length=400, null=True, blank=True)
+    foodtruck = models.ForeignKey(FoodTruck, on_delete=models.CASCADE)
+    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
 
 class Producto(models.Model):
-    tienda = models.ForeignKey(Tienda, on_delete=models.CASCADE, null=True)
-    nombre = models.CharField(max_length=255, null=True)
-    descripcion = models.TextField(null=True)
-    precio = models.DecimalField(max_digits=10, decimal_places=2, null=True)
-    imagen = models.CharField(max_length=50, null=True)
+    nombre = models.CharField(max_length=255, null=True, blank=True)
+    descripcion = models.TextField(null=True, blank=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    imagen = models.CharField(max_length=50, null=True, blank=True)
+    categoria = models.CharField(max_length=20, null=True, blank=True)
+    foodtruck = models.ForeignKey(FoodTruck, on_delete=models.CASCADE)
 
 class DetallesPedido(models.Model):
-    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE, null=True)
-    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, null=True)
-    cantidad = models.IntegerField(null=True)
-    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    cantidad = models.IntegerField(null=True, blank=True)
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
